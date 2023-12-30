1b53ed76a84c7056fca9fb909df7debabd5a6c4d
chameleon05119 <chameleon05119@gmail.com>
2023-12-30 10:21:55 +0900

    ヒーローに画像を追加できるように修正

--- components/hero.js
+++ components/hero.js
@@ -1,8 +1,9 @@
-export default function Hero({ title, subtitle }) {
+export default function Hero({ title, subtitle, imageOn = false }) {
   return (
     <div>
       <h1>{title}</h1>
       <h2>{subtitle}</h2>
+      {imageOn && <figure> [画像] </figure>}
     </div>
   );
 }
