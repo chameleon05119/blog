2f9cac98ce2c925ac009263a6dfbb03a7e324877
chameleon05119 <chameleon05119@gmail.com>
2023-12-30 20:29:14 +0900

    フッターのスタイルを追加

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,13 @@
 import Logo from "@/components/logo";
+import style from "styles/footer.module.css";
 
 export default function Footer() {
   return (
-    <footer>
-      <Logo />
+    <footer className={style.wrapper}>
+      <div className={style.flexContainer}>
+        <Logo />
+        [ソーシャル]
+      </div>
     </footer>
   );
 }
