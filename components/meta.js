784d11fd46e016c1449893921e315ae2c01c746f
chameleon05119 <chameleon05119@gmail.com>
2023-12-31 11:29:14 +0900

    その他のメタ情報を追加

--- components/meta.js
+++ components/meta.js
@@ -25,6 +25,11 @@ export default function Meta({ pageTitle, pageDesc }) {
       <meta property="og:description" content={desc} />
       <link rel="canonical" href={url} />
       <meta property="og:url" content={url} />
+      <meta property="og:site_name" content={siteTitle} />
+      <meta property="og:type" content={siteType} />
+      <meta property="og:local" content={siteLocale} />
+      <link rel="icon" href={siteIcon} />
+      <link rel="apple-touch-icon" href={siteIcon} />
     </Head>
   );
 }
