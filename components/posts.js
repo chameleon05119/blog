734dc82aa9aa064aeeea9924ccd278de1abf3198
chameleon05119 <chameleon05119@gmail.com>
2024-01-02 12:11:47 +0900

    記事一覧のスタイルをグリッドレイアウトに変更

--- components/posts.js
+++ components/posts.js
@@ -6,16 +6,16 @@ export default function Posts({ posts }) {
   return (
     <div className={styles.gridContainer}>
       {posts.map(({ title, slug, eyecatch }) => (
-        <article className={styles.posts} key={slug}>
+        <article className={styles.post} key={slug}>
           <Link href={`/blog/${slug}`}>
             <a>
               <figure>
                 <Image
                   src={eyecatch.url}
                   alt=""
-                  layout="responsive"
-                  width={eyecatch.width}
-                  height={eyecatch.height}
+                  layout="fill"
+                  objectFit="cover"
+                  sizes="(min-width: 1152px) 576px, 50vw"
                   placeholder="blur"
                   blurDataURL={eyecatch.blurDataURL}
                 />
