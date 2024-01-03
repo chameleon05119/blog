40bad3cc467e33eb6858ea207609ed45c2a54bd6
chameleon05119 <chameleon05119@gmail.com>
2024-01-03 15:16:20 +0900

    カテゴリページに記事一覧を追加

--- pages/blog/category/[slug].js
+++ pages/blog/category/[slug].js
@@ -1,11 +1,15 @@
-import { getAllCategories } from "lib/api";
+import { getAllCategories, getAllPostsByCategory } from "lib/api";
 import Container from "@/components/container";
 import PostHeader from "@/components/post-header";
+import Posts from "@/components/posts";
+import { getPlaiceholder } from "plaiceholder";
+import { eyechatchLocal } from "lib/constants";
 
-export default function Category({ name }) {
+export default function Category({ name, posts }) {
   return (
     <Container>
       <PostHeader title={name} subtitle="Blog Category" />
+      <Posts posts={posts} />
     </Container>
   );
 }
@@ -23,9 +27,21 @@ export async function getStaticProps(context) {
   const allCats = await getAllCategories();
   const category = allCats.find(({ slug }) => slug === catSlug);
 
+  const posts = await getAllPostsByCategory(category.id);
+  console.log(posts);
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty("eyecatch")) {
+      post.eyecatch = eyechatchLocal;
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url);
+    post.eyecatch.blurDataURL = base64;
+  }
+
   return {
     props: {
       name: category.name,
+      posts: posts,
     },
   };
 }
