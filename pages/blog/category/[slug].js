5b9b1ac0cf8a29cb509205998edb9ccdf5659cc8
chameleon05119 <chameleon05119@gmail.com>
2024-01-03 15:04:44 +0900

    カテゴリ一覧から全てのカテゴリページを生成する

--- pages/blog/category/[slug].js
+++ pages/blog/category/[slug].js
@@ -5,27 +5,27 @@ import PostHeader from "@/components/post-header";
 export default function Category({ name }) {
   return (
     <Container>
-      <PostHeader title={name} subtitle="Blog Categor" />
+      <PostHeader title={name} subtitle="Blog Category" />
     </Container>
   );
 }
 
 export async function getStaticPaths() {
+  const allCats = await getAllCategories();
   return {
-    paths: ["/blog/category/techology"],
+    paths: allCats.map(({ slug }) => `/blog/category/${slug}`),
     fallback: false,
   };
 }
 
 export async function getStaticProps(context) {
   const catSlug = context.params.slug;
-
   const allCats = await getAllCategories();
-  const cat = allCats.find(({ slug }) => slug === catSlug);
+  const category = allCats.find(({ slug }) => slug === catSlug);
 
   return {
     props: {
-      name: cat.name,
+      name: category.name,
     },
   };
 }
