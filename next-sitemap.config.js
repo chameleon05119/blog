260b41fd42a073fe93912c11117302138f5032c5
chameleon05119 <chameleon05119@gmail.com>
2024-01-06 10:17:57 +0900

    動的サイトマップを生成できるようにする

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,8 @@
 /** @type {import("next-sitemap").IConfig} */
 const config = {
   siteUrl: "https://blog-sigma-umber.vercel.app/",
-  outDir: "./out",
+  exclude: ["/server-sitemap.xml"],
+  robotsTxtOptions: { additionalSitemaps: ["https://xxx/server-sitemap.xml"] },
 };
 
 module.exports = config;
