cd376bb23584221de3179d32012bd267fe148fa6
chameleon05119 <chameleon05119@gmail.com>
2024-01-06 12:23:34 +0900

    サイトマップ生成周りの変更を削除

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import("next-sitemap").IConfig} */
-const config = {
-  siteUrl: "https://blog-sigma-umber.vercel.app/",
-  exclude: ["/server-sitemap.xml"],
-  robotsTxtOptions: { additionalSitemaps: ["https://xxx/server-sitemap.xml"] },
-};
-
-module.exports = config;
